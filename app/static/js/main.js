4921feec44172ba9531fa7d787668e94386019e6
IDEAPAD320\Scur <evgeny.ocheredko@gmail.com>
2019-05-27 23:02:27 +0300

    Added some validation to signUp form

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,6 +1,19 @@
-let app = new Vue({
-    el: '#app',
-    data: {
-        message: 'Hello Vue!'
-    }
+var form = new Vue({
+	el: '#signUpForm',
+	data: {
+		errors: [],
+		pass: null,
+		repass: null,
+		passRegExp: new RegExp("^(((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})")
+},
+methods: {
+	checkForm: function(e) {
+		e.preventDefault();
+		this.errors = [];
+		if (!this.pass.match(this.passRegExp))
+			this.errors.push("Password must be at least of length 6 and contain letters and digits");
+		else if (this.repass !== this.pass)
+			this.errors.push("Password and Re-password must be equal");
+	}
+}
 });
