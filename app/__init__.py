e13e1a0d250e383f701c53f9d3871ff7d428f30e
o4eredko <evgeny.ocheredko@gmail.com>
2019-06-05 17:46:00 +0300

    Required plugins list created

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,12 @@ class CustomFlask(Flask):
         variable_start_string='%%',  # Default is '{{', I'm changing this because Vue.js uses '{{' / '}}'
         variable_end_string='%%',
     ))
+
+
 app = CustomFlask(__name__)
+app.config["host"] = "remotemysql.com"
+app.config["user"] = "EbumYmCv3K"
+app.config["password"] = "8tdbKY8Vct"
+app.config["db"] = "EbumYmCv3K"
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from flask import render_template
 from app import app
+from .database import Database
 
 @app.route('/')
 @app.route('/index')
-def hello_world():
-	return render_template("index.html", username="fads")
+def index():
+	db = Database()
+	return render_template("index.html")
